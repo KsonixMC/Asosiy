2a7188fa2c01773b5ec829593040d2a447015c19
KsonixMC <xondamirtojiboyev09@gmail.com>
2026-09-04 21:05:59 +0500

    Update Matrix.py

--- Dars/Revision/Matrix.py
+++ Dars/Revision/Matrix.py
@@ -1,5 +1,21 @@
 import random
 
+
+
+m = int(input("m="))
+n = int(input('n='))
+
+mas = []
+for i in range(m):
+    row = []
+    for q in range(n):
+        row.append(random.randint(0,9))
+    mas.append(row)
+[print(row) for row in mas]
+print('*'*20)
+
+
+
 # matrix3
 
 # m = int(input('m='))
@@ -14,20 +30,38 @@ import random
 
 # matrix6
 
-m = int(input('m='))
-n = int(input('n='))
-q = int(input('q='))
+##m = int(input('m='))
+##n = int(input('n='))
+##v = int(input('q='))
+##
+##a = []
+##for i in range(m):
+##    a.append(random.randint(0,9))
+##print(a)
+##print('*'*20)
+##
+##b = []
+##
+##for i in range(m):
+##    row = []
+##    for q in range(n):
+##        if q == 0:
+##            row.append(a[i])
+##        else:
+##            z = row[q-1]*v
+##            row.append(z)
+##    b.append(row)
+##[print(row) for row in b]
 
-a = []
-for i in range(m):
-    a.append(random.randint(0,9))
-print(a)
-print('*'*20)
+##matrix9
+
+##for x in range(0, m, 2):
+##    print(mas[x])
+
+##matrix12
 
 b = []
-for i in range(n):
-    row = []
-    for x in range(1, m):
-        row.append(a[i]*10)
-    print(row)
-[print(row) for row in b]
+for y in range(n):
+    row1 = []
+    for x in range(m):
+        row1.append(
