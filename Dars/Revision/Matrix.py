fa0ec25e258cd39fc43f2d7aaf7f857c99b9548b
KsonixMC <xondamirtojiboyev09@gmail.com>
2026-09-05 17:01:19 +0500

    smth

--- Dars/Revision/Matrix.py
+++ Dars/Revision/Matrix.py
@@ -1,18 +1,25 @@
 import random
+def print2(massive):
+    print('_' * ((len(str(massive[0]))-str(massive[0]).count(','))+2))
 
+    for line in massive:
+        print('|', end=' ')
+        [print(x, end=' ') for x in line]
+        print('|')
+    print('-' * ((len(str(massive[0]))-str(massive[0]).count(','))+2))
 
-
-m = int(input("m="))
-n = int(input('n='))
+m = int(input("qator="))
+n = int(input('ustun='))
 
 mas = []
 for i in range(m):
     row = []
     for q in range(n):
-        row.append(random.randint(0,9))
+        row.append(random.randint(0,1))
     mas.append(row)
-[print(row) for row in mas]
-print('*'*20)
+# [print(row) for row in mas]
+print2(mas)
+
 
 
 
@@ -60,8 +67,47 @@ print('*'*20)
 
 ##matrix12
 
-b = []
-for y in range(n):
-    row1 = []
-    for x in range(m):
-        row1.append(
+# b = []
+#
+# for y in range(n):
+#     row1 = []
+#     for x in range(m):
+#         row1.append(mas[x][y])
+#     b.append(row1)
+# [print(row1) for row1 in b]
+#
+# for i in range(n):
+#     print('i =', i)
+#     if i%2==1:
+#         for q in range(m-1, -1, -1):
+#             # print('q =', q)
+#             print(b[i][q])
+#     else:
+#         for q in range(m):
+#             print(b[i][q])
+
+# matrix15
+
+# for y in range(m//2+1):
+    # for x in range(y, m-y):
+    #     print(mas[y][x], end=' ')
+
+# for y in range(m-1, -1, -1):
+#     for x in range(m-y-1, y+1):
+#         print(mas[x][y], end=' ')
+
+for asosiy in range((min(m,n)+1)//2):
+    # print("---")
+    for tepa in range(0+asosiy, n-asosiy):
+        print(mas[asosiy][tepa], end=' ')
+    # print("J")
+    for ung in range(1+asosiy, m-asosiy):
+        print(mas[ung][n-1-asosiy], end=' ')
+    # print("___")
+    for past in range(n-1-asosiy-1,-1+asosiy,-1):
+        print(mas[m-1-asosiy][past], end=' ')
+        # print(mas[past][m-1-asosiy])
+    # print('L')
+    for chap in range(m-1-asosiy-1,0+asosiy,-1):
+        print(mas[chap][0+asosiy], end=' ')
+    # print('/')
