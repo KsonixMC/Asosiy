c703d2dc2ff88c2a445d8fb7ace00c360ab8ca56
KsonixMC <xondamirtojiboyev09@gmail.com>
2026-09-11 11:31:17 +0500

    update

---
+++ Dars/Revision/FunSimple.py
@@ -0,0 +1,63 @@
+# FunSimple1
+
+# def PowerA3(a):
+#     a = a**3
+#     return a
+#
+# A = float(input('A = '))
+# B = float(input('B = '))
+# C = float(input('C = '))
+# D = int(input('D = '))
+# E = int(input('E = '))
+#
+# print(PowerA3(A))
+# print(PowerA3(B))
+# print(PowerA3(C))
+# print(PowerA3(D))
+# print(PowerA3(E))
+
+# FunSimple2
+
+# def PowerA234(v):
+#     a = v**2
+#     b = v**3
+#     c = v**4
+#     return a, b, c
+#
+# A = float(input('A = '))
+# B = float(input('B = '))
+# C = float(input('C = '))
+#
+# print(PowerA234(A))
+# print(PowerA234(B))
+# print(PowerA234(C))
+
+# FunSimple3
+
+# def MEAN(v1, v2):
+#     a = (v1+v2)/2
+#     g = (v1*v2)**.5
+#
+#     return a, g
+#
+# A = int(input('A = '))
+# B = int(input('B = '))
+# C = int(input('C = '))
+# D = int(input('D = '))
+#
+# print(MEAN(A, B))
+# print(MEAN(A, C))
+# print(MEAN(A, D))
+
+# FunSimple4
+
+# def Triangle(v):
+#     S = (3**.5)/4*v**2
+#     P = 3*v
+#
+#     return S, P
+# a = int(input('tomon = '))
+# print(Triangle(a))
+
+# FunSimple5
+
