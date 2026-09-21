5b29e98139e0abfa46adc21696879b1749bec834
KsonixMC <xondamirtojiboyev09@gmail.com>
2026-09-21 11:47:12 +0500

    Update String.py

---
+++ Kurs/2-kurs/String.py
@@ -0,0 +1,134 @@
+# string1
+#
+# n = input()
+# print(ord(n))
+
+# string2
+
+# n = int(input('n = '))
+# print(chr(n))
+
+# string3
+
+# n = input('n = ')
+# print(chr(ord(n)-1))
+# print(chr(ord(n)+1))
+
+# string4
+
+# n = int(input('n = '))
+# mas= []
+# for i in range(65, 91):
+#     mas.append(chr(i))
+#
+# for i in range(n):
+#     print(mas[i])
+
+# string5
+
+# n = int(input('n = '))
+# mas=[]
+# for i in range(122, 98, -1):
+#     mas.append(chr(i))
+# for i in range(n):
+#     print(mas[i])
+
+# string6
+
+# n = input('n = ')
+# if 48 <= ord(n) <= 57:
+#     print('digit')
+# elif 65 <= ord(n) <= 90:
+#     print('lotin')
+# elif 97 <= ord(n) <= 122:
+#     print('lotin')
+# else:
+#     print(0)
+
+# string7
+
+# satr = input()
+# print(ord(satr[0]), ord(satr[-1]))
+
+# string8
+
+# n = int(input('n = '))
+# s = input()
+# print(s*n)
+
+# string9
+
+# s1 = input()
+# s2 = input()
+# print(s1 + s2)
+
+# string10
+
+# s = input()
+# mas = []
+# for i in range(len(s)-1, -1, -1):
+#     mas.append(s[i])
+# S = ''
+# for i in range(len(s)):
+#     S += mas[i]
+# print(S)
+
+# string11
+
+# s = input()
+# S = ' '.join(s)
+# print(S)
+
+# string12
+
+# n = int(input('n = '))
+# s = input()
+# smbl = '*'*n
+# print(smbl.join(s))
+
+# string13
+
+# s = input()
+# c = 0
+# for i in range(len(s)):
+#     if 48 <= ord(s[i]) <= 57:
+#         c += 1
+# print(c)
+
+# string14
+
+# s = input()
+# c = 0
+# for i in range(len(s)):
+#     if 65 <= ord(s[i]) <= 90:
+#         c += 1
+# print(c)
+
+# string15
+
+# a = input()
+# b = 0
+# for i in range(len(a)):
+#     if a[i] in 'qwertyuiopasdfghjklzxcvbnmйцукенгшщзхъфывапролджэячсмитьбю':
+#         b += 1
+# print(b)
+
+# string16
+
+# s = input()
+# print(s.lower())
+
+# string17
+
+# s = input()
+# print(s.lower())
+
+# string18
+
+# s = input()
+# print(s.swapcase())
+
+# string19
+
+s = input()
+if  
